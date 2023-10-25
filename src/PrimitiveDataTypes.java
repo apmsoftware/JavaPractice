fc52f31ec72dde3fc0f4524f633ce9db987ef134
apmsoftware <anishprashantmisal@gmail.com>
2023-10-25 15:19:11 +0530

    Aarya: Initial Commit

---
+++ src/PrimitiveDataTypes.java
@@ -0,0 +1,25 @@
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        System.out.println("Below are Primitive  Data types");
+        // Boolean - 1 bit
+        boolean bool = true;
+
+        //Numeric Types
+
+        //Char Type 2 Byte
+        char cha = 'A';
+
+        // Integral Type
+        byte b = 0x0F;  // 15  1 Byte = 8 bits
+        short s = 2;      // 2 Byte
+        int i = 10;     // 4 Byte
+        long l = 56543434; // 8 Byte
+
+        //Floating Point
+
+        float f = 12.5f;     // 4 Byte
+        double d = 12.897d; // 8 Byte
+
+
+    }
+}
