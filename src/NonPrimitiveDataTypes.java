4a58e878d95d2d3b17b7e8cfd6bc4e5f8fd5c6b5
apmsoftware <anishprashantmisal@gmail.com>
2023-10-25 17:23:47 +0530

    Aarya:Commit for NonPrimitiveDataTypes.java

---
+++ src/NonPrimitiveDataTypes.java
@@ -0,0 +1,32 @@
+public class NonPrimitiveDataTypes {
+    public static void main(String[] args) {
+
+        System.out.println("Below are Non Premitive Datatypes");
+
+        // Declare a string
+        String s = "Aarya";
+        String str = new String("Anish");
+
+        // Declare an Array
+
+        int[] intArray = new int[5];
+        intArray[0]=1;
+        intArray[1]=2;
+        intArray[2]=1;
+        intArray[3]=1;
+        intArray[4]=1;
+        System.out.println("Value at 1st index: "+ intArray[1]);
+
+        String[] strArray =new String[6];
+        strArray[0]="Aarya";
+        strArray[3]="Aavya";
+
+        Double[] doubleArray = new Double[7];
+        doubleArray[0]=12.0;
+        doubleArray[1]=13.3;
+
+
+        Boolean[] boolArray = new Boolean[6];
+
+    }
+}
